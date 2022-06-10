adde4c69d010cfbb06a937399c3c0e1cf8db1764
Evans Rutto <robbyevans001@gmail.com>
2022-06-10 18:06:07 +0300

    commit tree 1263e8ed0283f817cd63e149cb2bd66edd2c75a6

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution here
+// first function findMatching()
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+function findMatching(array,name){
+   const newArray=[];
+    for (let items of array){
+        if((items.toUpperCase())===(name.toUpperCase())){
+            newArray.push(items)
+        }
+    }
+    return newArray;
+}
+
+findMatching(drivers,'Bobby');
+
+// 2nd function fuzzyMatch()
+
+function fuzzyMatch(array,initial){
+    const newArray=[];
+    for(let items of array){
+        if(initial===(items.slice(0,2))){
+            newArray.push(items)
+        }
+    }
+    return newArray
+};
+
+fuzzyMatch(drivers,'Sa')
+
+// 3rd function matchName()
+
+function matchName(array,name){
+    const newArray=[];
+    for (let items of array){
+        if (items.name===name){
+            newArray.push(items);
+        }
+    }
+  return newArray;
+}
